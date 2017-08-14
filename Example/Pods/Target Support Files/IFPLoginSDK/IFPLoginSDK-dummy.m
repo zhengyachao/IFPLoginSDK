6a9ad5e0e785a7beb863753edf2b2e10b4100475
zhengyachao <15038253754@163.com>
2017-08-14 16:15:12 +0800

    IFPLoginSDK v0.1.0

---
+++ Example/Pods/Target Support Files/IFPLoginSDK/IFPLoginSDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_IFPLoginSDK : NSObject
+@end
+@implementation PodsDummy_IFPLoginSDK
+@end
---
+++ Example/Pods/Target Support Files/Pods-IFPLoginSDK_Example/Pods-IFPLoginSDK_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IFPLoginSDK_Example : NSObject
+@end
+@implementation PodsDummy_Pods_IFPLoginSDK_Example
+@end
